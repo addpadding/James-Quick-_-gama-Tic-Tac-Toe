6e0939c9a5cd635ac066e25ed86766b6a7abff61
addpadding <tiktokaddison20@gmail.com>
2023-03-31 05:30:38 +0300

    58.4%

--- js/58%_.js
+++ js/58%_.js
@@ -1,8 +1,12 @@
-console.log("45%");
+console.log("58");
 // ============================
 
 var box = Array.from(document.querySelectorAll(".box"));
-console.log(box);
+
+var spaces = [null, null, null, null, null, null, null, null, null];
+var o_Text = "O";
+var x_Text = "X";
+var current = o_Text;
 
 function draw_Bord() {
     box.forEach(function (box, index) {
@@ -23,8 +27,22 @@ function draw_Bord() {
         if (index > 5) {
             style_string += "border-top: 3px solid #3e0249;";
         }
-        box.style = style_string
+
+        box.style = style_string;
+
+        box.addEventListener("click", box_clicked);
     });
 }
-
 draw_Bord();
+
+function box_clicked(e) {
+    var id = e.target.id
+    console.log(id)
+
+    if (!spaces[id]) {
+        spaces[id] = current
+        e.target.innerText = current
+
+        current = current === o_Text ? x_Text : o_Text
+    }
+}
